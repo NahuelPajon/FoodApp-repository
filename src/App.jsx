e0025ce0dafff7f9f735bffe21ff9bd11987f216
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2025-05-12 21:09:19 -0300

    changes in class Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { FoodImage, FoodTable } from "./components/FoodTable/FoodTable";
+//import { FoodImage, FoodTable } from "./components/FoodTable/FoodTable";
 import { TitleHeader } from "./components/TitleHeader";
-import { OrderDetails } from "./components/OrderDetails/OrderDetails";
-import { useTheme } from "./context/ThemeContext";
-import { Routes } from "react-router-dom";
+//import { OrderDetails } from "./components/OrderDetails/OrderDetails";
+//import { useTheme } from "./context/ThemeContext";
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FoodDetails from "./pages/FoodDetails";
 // import { useTheme } from "./context/ThemeContext";
 // import styles from "./App.module.css";
 
 function App() {
-  const { darkMode } = useTheme(); //Traigo el contexto del tema (si es oscuro o claro)
-
-  const [foods, setFoods] = useState([]); // stored fetched foods
-  const [orders, setOrders] = useState([]); // stored orders
-  const [loading, setLoading] = useState(true); // loading state
-  const [error, setError] = useState(null); // error handling state
-
-  // Fetch de los FOODS desde el backend db.json
-  useEffect(() => {
-    const fetchFoods = async () => {
-      try {
-        const url = "http://localhost:3000/FOODS";
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setFoods(data); // actualiza el estado de foods
-        setOrders(data.map(({ id }) => ({ id, quantity: 0 }))); // inicializa los orders
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchFoods();
-  }, []);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = darkMode ? "#222" : "#f9f9f9";
-    document.body.style.color = darkMode ? "#fff" : "black";
-  }, [darkMode]);
-
-  const incrementCuenta = (id) => {
-    setOrders((prevOrders) =>
-      prevOrders.map((order) => {
-        const food = foods.find((f) => f.id === id);
-        if (order.id === id && food && order.quantity < food.stock) {
-          return { ...order, quantity: order.quantity + 1 };
-        }
-        return order;
-      })
-    );
-  };
-
-  const decrementCuenta = (id) => {
-    setOrders((prevOrders) =>
-      prevOrders.map((order) => {
-        const food = foods.find((f) => f.id === id);
-        if (order.id === id && food && order.quantity > 0) {
-          return { ...order, quantity: order.quantity - 1 };
-        }
-        return order;
-      })
-    );
-  };
-
-  const deleteCuenta = (id) => {
-    setOrders((prevOrders) =>
-      prevOrders.map((order) => {
-        if (order.id === id) {
-          return { ...order, quantity: 0 };
-        }
-        return order;
-      })
-    );
-  };
-
   return (
-    <div>
+    <>
       <TitleHeader />
-      <FoodTable foods={foods} incrementCuenta={incrementCuenta} decrementCuenta= {decrementCuenta} orders={orders} />
-      <OrderDetails
-        foods={foods}
-        orders={orders}
-        incrementCuenta={incrementCuenta}
-        decrementCuenta={decrementCuenta}
-        deleteCuenta={deleteCuenta}
-      />
-    </div>
+      <Routes>
+        {/* you can use 👇 character (* or wildcard) to any other route
+        in combination with the Navigate component you can redirect the user to a default page */}
+        <Route path="/*" element={<Navigate replace to="/foods" />} />
+
+        {/* You can compose the routes the routes wrapping the Route components */}
+          
+        {/* </Route> */}
+        <Route path="/foods/*" element={<HomePage />}/>
+        <Route path="/foods/details" element={<FoodDetails />} />
+        {/* <Route path="foods/details/:productId" element={<FoodDetails />} /> */}
+      </Routes>
+    </>
   );
 }
 
